c8e7bbffa12227b7eeffe90ee9af220624a07df2
cicidoll <ayaseemt@qq.com>
2020-12-25 18:54:37 +0800

    setting spider production mode

--- spider/initNew.py
+++ spider/initNew.py
@@ -12,7 +12,7 @@ class netSpider:
   def __init__(self):
     self.baseUrl = 'http://121.194.213.115/swyt/jxcdkbcx.php' #基于校园内网地址进行爬取
     self.ua = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'
-    self.week = 10# http://121.194.213.72/ 教务处网站 获取实时教学周
+    self.week = 16# http://121.194.213.72/ 教务处网站 获取实时教学周
     self.urlDic = {
                     'xnxq':"'2020-20211'"#查询日期,默认为2020-2021第一学期
                     #'jxcdmc'为教室get请求头
@@ -63,31 +63,31 @@ class netSpider:
                                    '502': [],'503': [],'504': [],'505': [],'506': [],'509': [] 
                                   } }
     }
-    # # 铸剑楼教室
-    # self.classRoomNumZhuJian = ['101','102','104','105','106','108','110','111','112',
-    #                             '201','202','204','205','206','207','208','210',
-    #                             '301','302','303','304','305','306','307','308','309',
-    #                             '401','402','403','404','405','406','407','408','409','410','411',
-    #                             '501','502','503','504','505','506','507','508','509','510','511' ]
-    # # 中楼教室
-    # self.classRoomNumZhong = ['103','104','107','110','112','113',
-    #                           '203','204','205','206','207','208','210','211',
-    #                           '303','304','305','306','307','308',
-    #                           '407','408',
-    #                           '503','504','505','506','507','510',
-    #                           '603','607',
-    #                           '703','704','705','707','708']
-    # # 西配楼教室                          
-    # self.classRoomNumXi = [ '102','103','104','105','106','109',
-    #                         '202','203','204','205','206','209',
-    #                         '302','303','304','305','306','309',
-    #                         '402','403','404','405','406','409',
-    #                         '502','503','504','505','506','509',]
+    # 铸剑楼教室
+    self.classRoomNumZhuJian = ['101','102','104','105','106','108','110','111','112',
+                                '201','202','204','205','206','207','208','210',
+                                '301','302','303','304','305','306','307','308','309',
+                                '401','402','403','404','405','406','407','408','409','410','411',
+                                '501','502','503','504','505','506','507','508','509','510','511' ]
+    # 中楼教室
+    self.classRoomNumZhong = ['103','104','107','110','112','113',
+                              '203','204','205','206','207','208','210','211',
+                              '303','304','305','306','307','308',
+                              '407','408',
+                              '503','504','505','506','507','510',
+                              '603','607',
+                              '703','704','705','707','708']
+    # 西配楼教室                          
+    self.classRoomNumXi = [ '102','103','104','105','106','109',
+                            '202','203','204','205','206','209',
+                            '302','303','304','305','306','309',
+                            '402','403','404','405','406','409',
+                            '502','503','504','505','506','509',]
 
     #测试用教室号
-    self.classRoomNumZhuJian = ['104']#铸剑楼教室
-    self.classRoomNumZhong = ['103']#中楼教室
-    self.classRoomNumXi = ['102']#西配楼教室
+    # self.classRoomNumZhuJian = ['104']#铸剑楼教室
+    # self.classRoomNumZhong = ['103']#中楼教室
+    # self.classRoomNumXi = ['102']#西配楼教室
     # 具体课表在网页中的路径表示
     self.pathPool = [#上午1、2节
                     "//table[@class='table table-bordered table-striped table-condensed']//tr[1]//td[2]/text()",
@@ -235,19 +235,24 @@ class netSpider:
                   # 1、需要记录数据如下：
                   for index in range(mobilizeTimes):
                       pathContent = htmlContent.xpath(pathMobilize)[index]
-                      # 原教学周索引为9，现教学周索引为15
-                      oldWeek = int(decodeHtml(pathContent[9])[4:-6])
-                      newWeek = int(decodeHtml(pathContent[15])[4:-6])
-                      # 检测原教学周与现教学周若早于当前教学周，直接跳过该组数据。
-                      if (self.week >= max(oldWeek,newWeek)): continue
                       className = pathContent[4][0].xpath('string(.)') # 课程名字
                       classes = pathContent[7].xpath('string(.)') # 调课类别
                       oldDate = pathContent[8].xpath('string(.)') # 原上课日期
                       oldTimes = pathContent[11].xpath('string(.)') # 原节次
                       oldRoom = pathContent[12][0].xpath('string(.)') # 原教室
-                      newDate = pathContent[14].xpath('string(.)') # 现上课日期
-                      newTimes = pathContent[17].xpath('string(.)') # 现节次
-                      newRoom = pathContent[18][0].xpath('string(.)') # 现教室
+                      newDate = '' # 置空
+                      newTimes = '' # 置空
+                      newRoom = '' # 置空
+
+                      if classes != '停课':
+                          # 原教学周索引为9，现教学周索引为15
+                          oldWeek = int(pathContent[9].xpath('string(.)'))
+                          newWeek = int(pathContent[15].xpath('string(.)'))
+                          # 检测原教学周与现教学周若早于当前教学周，直接跳过该组数据。
+                          if (self.week >= max(oldWeek,newWeek)): continue
+                          newDate = pathContent[14].xpath('string(.)') # 现上课日期
+                          newTimes = pathContent[17].xpath('string(.)') # 现节次
+                          newRoom = pathContent[18][0].xpath('string(.)') # 现教室
                       self.mobilizeBorrowJsonText["mobilize"][roomSelect][roomNumSelect[classRoomIndex]] \
                         .append( \
                             { 'className': className, \
@@ -276,8 +281,8 @@ class netSpider:
 
   def init(self, flag):
       #flag 控制行为：true-查询课时空闲 false-查询调停课信息
-      roomList = [self.classRoomNumZhuJian] #测试用
-      # roomList = [self.classRoomNumZhuJian, self.classRoomNumZhong, self.classRoomNumXi]
+      # roomList = [self.classRoomNumZhuJian] #测试用
+      roomList = [self.classRoomNumZhuJian, self.classRoomNumZhong, self.classRoomNumXi]
       for roomNumSelect in roomList:
           self.getResponse(roomNumSelect, flag)    
       # 将得到的数据保存为本地json文件
